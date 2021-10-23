ab9f567b7f4f4c64ad7c29c4967835f68d5e93ff
Demeco <Demeco_Bell@Demecos-MBP.attlocal.net>
2021-10-23 08:36:47 -0400

    add where I belong algo

--- algos.rb
+++ algos.rb
@@ -9,9 +9,8 @@ const oddOnly = (numberList) => {
   oddOnly([2, 3, 8, 10, 9, 4, 24, 18, 14, 13]);
 
 
-  ________________________________________________________________________________________________________________________
 
-  #I am Ruby
+#   #I am Ruby
 def oddOnly(numberList)
     allNumbers = numberList.join("\n")
     oddNumbers = allNumbers.scan(/[13579]/)
@@ -20,3 +19,28 @@ puts oddNumbers
 end
 
 oddOnly([2, 3, 8, 10, 9, 4, 24, 18, 14, 13])
+
+_____________________________________________________________________________________________________________________________
+
+#I am JavaScript
+function getIndexToIns(arr, num) {
+  arr.push(num);
+  function sortedNumbers (a, b) {
+  return a - b;
+  }
+  arr.sort(sortedNumbers);
+  return arr.indexOf(num);
+}
+
+getIndexToIns([40, 60], 50);
+
+
+  #I am Ruby
+def getIndexToIns(arr, num)
+ arr.push(num)
+sorted = arr.sort()
+puts sorted.find_index(num)
+
+end
+
+getIndexToIns([40, 60], 50);
