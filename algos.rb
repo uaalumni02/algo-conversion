54ccecadce37d2824e01f07e31590815e0ab7aa5
Demeco <Demeco_Bell@Demecos-MBP.attlocal.net>
2021-10-23 09:06:10 -0400

    add slasher algo

--- algos.rb
+++ algos.rb
@@ -1,46 +1,60 @@
 #I am JavaScript
-const oddOnly = (numberList) => {
-    const pattern = /^\d*[13579]$/gm;
-    const allNumbers = numberList.join('\n');
-    const oddNumbers = allNumbers.match(pattern);
-    return oddNumbers;
-  }
+# const oddOnly = (numberList) => {
+#     const pattern = /^\d*[13579]$/gm;
+#     const allNumbers = numberList.join('\n');
+#     const oddNumbers = allNumbers.match(pattern);
+#     return oddNumbers;
+#   }
   
-  oddOnly([2, 3, 8, 10, 9, 4, 24, 18, 14, 13]);
+#   oddOnly([2, 3, 8, 10, 9, 4, 24, 18, 14, 13]);
 
 
 
 #   #I am Ruby
-def oddOnly(numberList)
-    allNumbers = numberList.join("\n")
-    oddNumbers = allNumbers.scan(/[13579]/)
-puts oddNumbers
+# def oddOnly(numberList)
+#     allNumbers = numberList.join("\n")
+#     oddNumbers = allNumbers.scan(/[13579]/)
+# puts oddNumbers
     
-end
+# end
 
-oddOnly([2, 3, 8, 10, 9, 4, 24, 18, 14, 13])
+# oddOnly([2, 3, 8, 10, 9, 4, 24, 18, 14, 13])
 
-_____________________________________________________________________________________________________________________________
 
 #I am JavaScript
-function getIndexToIns(arr, num) {
-  arr.push(num);
-  function sortedNumbers (a, b) {
-  return a - b;
-  }
-  arr.sort(sortedNumbers);
-  return arr.indexOf(num);
-}
+# function getIndexToIns(arr, num) {
+#   arr.push(num);
+#   function sortedNumbers (a, b) {
+#   return a - b;
+#   }
+#   arr.sort(sortedNumbers);
+#   return arr.indexOf(num);
+# }
 
-getIndexToIns([40, 60], 50);
+# getIndexToIns([40, 60], 50);
 
 
   #I am Ruby
-def getIndexToIns(arr, num)
- arr.push(num)
-sorted = arr.sort()
-puts sorted.find_index(num)
+# def getIndexToIns(arr, num)
+#  arr.push(num)
+# sorted = arr.sort()
+# puts sorted.find_index(num)
+
+# end
+
+# getIndexToIns([40, 60], 50);
+
+#I am JavaScript
+# function slasher(arr, howMany) {
+#   return arr.slice(howMany);
+# }
+
+# slasher([1, 2, 3], 2);
+
+#I am Ruby
+def slasher(arr, howMany)
+  puts arr.slice!(howMany)
 
 end
 
-getIndexToIns([40, 60], 50);
+slasher([1, 2, 3], 2);
