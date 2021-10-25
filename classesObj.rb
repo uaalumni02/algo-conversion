18abfe70eb370e139441af2b1a5fb3ee51000eb1
Demeco <Demeco_Bell@Demecos-MBP.attlocal.net>
2021-10-25 19:01:00 -0400

    loop practice

---
+++ classesObj.rb
@@ -0,0 +1,10 @@
+class Book 
+    attr_accessor :title, :author, :pages
+
+end
+
+book1 = Book.new()
+
+book1.title = "Harry Potter"
+book1.author = "JK Rowling"
+book1.pages = 400
---
+++ loops.rb
@@ -0,0 +1,20 @@
+a = 1
+
+# while a <= 10
+# puts a
+# a += 1
+# end
+
+# for i in 1..10 do
+#     puts i
+
+# end
+
+loop do 
+puts a
+
+if a >= 10
+break 
+else a += 1
+end
+end
